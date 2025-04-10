3f813ba8829fc31b889df452303ad604e9d408ce
Benoit Goudreault-Emond <bgoudreault-emond@sailthru.com>
2025-04-10 13:12:22 -0400

    fix: [LT-1509] Added proper locking and testability to the ApiFactory.
    1. Lock inside computeIfAbsent() because those lambdas can run in parallel
       but they all munge with the same static...
    2. Add a way to customize creation of ApiClient (visible for testing only)
       so we can mock it and verify that the cache properly locks etc.
    3. Tests check that there are never any concurrent executions of the cache
       population lambda.

--- src/main/java/com/sailthru/sqs/ApiFactory.java
+++ src/main/java/com/sailthru/sqs/ApiFactory.java
@@ -11,6 +11,8 @@ import software.amazon.awssdk.utils.StringUtils;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.BiFunction;
 
 public class ApiFactory {
     private static final String HTTP_POOL_KEEPALIVE_SECONDS = "HTTP_POOL_KEEPALIVE_SECONDS";
@@ -20,20 +22,45 @@ public class ApiFactory {
             safeParseInt(System.getenv(HTTP_POOL_KEEPALIVE_SECONDS), 300),
             TimeUnit.SECONDS))
         .build();
-    private static final Map<ClientApiDetails, EventsApi> CACHE = new ConcurrentHashMap<>();
+    // VisibleForTesting
+    static final Map<ClientApiDetails, EventsApi> CACHE = new ConcurrentHashMap<>();
+    private static final ReentrantLock lock = new ReentrantLock();
+
+    private BiFunction<String, String, ApiClient> apiClientFactory = ApiClient::new;
 
     public EventsApi of(final String apiKey, final String apiSecret, final String apiURL) {
         final ClientApiDetails apiDetails = new ClientApiDetails(apiKey, apiSecret, apiURL);
         return CACHE.computeIfAbsent(apiDetails, details -> {
-            ApiClient client = new ApiClient(details.apiKey(), details.apiSecret());
-            // use the same connection pool for everybody
-            client.configureFromOkclient(commonHttpClient);
-            client.getAdapterBuilder().baseUrl(details.apiURL());
-            return client.createService(EventsApi.class);
+            // we can only run one configuration segment at a time, globally, since ApiClient incorrectly
+            // makes its OkHttpClient.Builder a static instance - so if you have multiple threads trying
+            // to construct instances at the same time, you can run into trouble.
+            // This makes the concurrent map slightly less performant, but only if we're hitting contention
+            // due to multiple messages coming up at same time with different cache keys. In steady state
+            // the lock will not be used.
+            //
+            // once the EventsApi instance is returned, we're OK because the OkHttpClient.Builder instance
+            // was already called to obtain a new OkHttpClient instance, which is bound to the Retrofit
+            // object.
+            lock.lock();
+            try {
+                ApiClient client = apiClientFactory.apply(details.apiKey(), details.apiSecret());
+                // use the same connection pool for everybody
+                client.configureFromOkclient(commonHttpClient);
+                client.getAdapterBuilder().baseUrl(details.apiURL());
+                return client.createService(EventsApi.class);
+            } finally {
+                lock.unlock();
+            }
         });
     }
 
-    private record ClientApiDetails(String apiKey, String apiSecret, String apiURL) {
+    // VisibleForTesting
+    void setApiClientFactory(BiFunction<String, String, ApiClient> apiClientFactory) {
+        this.apiClientFactory = apiClientFactory;
+    }
+
+    // VisibleForTesting
+    record ClientApiDetails(String apiKey, String apiSecret, String apiURL) {
     }
 
     private static int safeParseInt(String value, int defaultValue) {
